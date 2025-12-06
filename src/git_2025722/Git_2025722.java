351a1adf95f9865de564c8a31238895fb4ccb2b7
Sergio Sanchez <sesnavas@gmail.com>
2025-12-06 17:45:07 +0000

    firstCommit to Git

---
+++ src/git_2025722/Git_2025722.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package git_2025722;
+
+/**
+ *
+ * @author ss_na
+ */
+public class Git_2025722 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
